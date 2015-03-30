329095944130adcadebacd153740893eb733f709
killer <killer>
2015-03-30 23:37:14 +0800

    unit test working

--- AT Command Script Processor/AtScriptProcessing.cs
+++ AT Command Script Processor/AtScriptProcessing.cs
@@ -4,12 +4,93 @@ using System.Text;
 
 namespace AT_Command_Script_Processor
 {
+
+
     public interface IAtScriptProcessing
     {
-
+        List<AtScriptProcessingData> ProcessArrayString(string[] strArray);
     }
+
+
+
+
+    public class AtScriptProcessingData
+    {
+        public bool IsSpecialCommand { get; set; }
+        public byte ByteToSend { get; set; }
+        public string ATCommandToSend { get; set; }
+        public static AtScriptProcessingData GetContainer()
+        {
+            var a = new AtScriptProcessingData();
+            a.IsSpecialCommand = false;
+            a.ATCommandToSend = null;
+            a.ByteToSend = 0;
+            return a;
+        }
+    }
+
+
 
     public class AtScriptProcessing : IAtScriptProcessing
     {
+        public List<AtScriptProcessingData> ProcessArrayString(string[] strArray)
+        {
+            var lstAtScriptData = new List<AtScriptProcessingData>();
+            
+
+            foreach (var x in strArray)
+            {
+                if (x.StartsWith(@"//"))
+                    continue;
+
+                string[] spliString = x.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                if (spliString.Length != 4)
+                    continue;
+
+                int delay = 0;
+                bool boolReadRx = false;
+                string ATcmd = "";
+
+                try
+                {
+                    delay = int.Parse(spliString[2].Trim());
+                    boolReadRx = Convert.ToBoolean(spliString[1].Trim());
+                    ATcmd = spliString[0].Trim();
+                }
+                catch
+                {
+                    continue;
+                }
+
+                var atScriptData = AtScriptProcessingData.GetContainer();
+
+                if (ATcmd.StartsWith("__@HEX")) // if special command
+                {
+                    atScriptData.IsSpecialCommand = true;
+
+                    try
+                    {
+                        string[] splitstr = ATcmd.Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
+                        byte ByToSend = Convert.ToByte(  splitstr[1].Trim(), 16);
+                        atScriptData.ByteToSend = ByToSend;
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    atScriptData.ATCommandToSend = ATcmd;
+                }
+
+                
+                lstAtScriptData.Add(atScriptData);
+            }
+
+            return lstAtScriptData;
+
+        }
+
     }
 }
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -11,7 +11,21 @@ namespace UnitTest
         public void ScriptProcessingTest()
         {
             IAtScriptProcessing iAtScriptProc = new AtScriptProcessing();
+            string [] arrayString = new string[]
+                {
+                    "//asdfasda",
+                    "__@HEX = 0xAA|True|500|This is a comment",
+                    "AT To Send|False|500|This is a comment"
+                };
 
+            var ret = iAtScriptProc.ProcessArrayString(arrayString);
+            Assert.AreEqual(2, ret.Count);
+
+            Assert.AreEqual(true, ret[0].IsSpecialCommand);
+            Assert.AreEqual(0xAA, ret[0].ByteToSend);
+
+            Assert.AreEqual(false, ret[1].IsSpecialCommand);
+            Assert.AreEqual("AT To Send", ret[1].ATCommandToSend);
         }
     }
 }
